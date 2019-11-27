2030003fa11767ecfae8c517f45ca522b68022ba
OperatorSDK Katacoda <operator-sdk@example.com>
2019-11-27 08:11:22 +0000

    updatedver

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<h1>My first PHP page by Nilesh</h1>
+<h1>My first PHP page by Nilesh/sudarshan</h1>
 
 <?php
 echo "First S2I Hello World ---Nowdyas I daily woke up with Sanjay Raut's Press Conference";
