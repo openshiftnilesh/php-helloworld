c03641540c4bd3713d8b7e95d501c07a86038055
openshiftnilesh <openshiftnilesh@gmail.com>
2019-12-04 04:57:16 +0000

    my first commit

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 <html>
 <body>
 
-<h1>My first PHP page by Nilesh/Sudarshan</h1>
+<h1>My first PHP page by Nilesh/Sudarshan Dec</h1>
 
 <?php
-echo "First S2I Hello World OpenShift";
+echo "First S2I Hello World OpenShift"
 ?>
 
 </body>
