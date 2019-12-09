e4b3195954b80494eea9b338a2a08cf6c2b5891b
OperatorSDK Katacoda <operator-sdk@example.com>
2019-12-09 06:32:41 +0000

    this is 3rd index.php version

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 <html>
 <body>
 
-<h1>My second PHP page by Nilesh/Sudarshan Dec</h1>
+<h1>My third PHP page by Nilesh/Sudarshan Dec</h1>
 
 <?php
-echo "First S2I Hello World OpenShift"
+echo "third S2I Hello World OpenShift"
 ?>
 
 </body>
