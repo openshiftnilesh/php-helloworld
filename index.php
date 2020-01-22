d5fd36b25ccfbf80499fc5a366f0e7e2fe85b02f
gr8nilesh@gmail.com <gr8nilesh@gmail.com>
2020-01-22 04:18:40 +0000

    my first commit

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<h1>My latest 2 PHP page by Nilesh/Sudarshan/Deepak 21Jan2020@08.00pmIST</h1>
+<h1>My latest 2 PHP page by Nilesh/Sudarshan/Deepak 22Jan2020</h1>
 
 <?php
 echo "latest S2I Hello World OpenShift"
