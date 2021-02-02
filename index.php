350c200d87a29ea17e27d6aef148735d6a605e40
santosh khade <santoshkkhade@gmail.com>
2021-02-02 15:43:34 +0000

    santosh

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<h1>My  2 PHP page by Nilesh/Sudarshan/Deepak 11April2020</h1>
+<h1>My  2 PHP page by Nilesh/Sudarshan/Deepak/Santosh 11April2020</h1>
 
 <?php
 echo "latest S2I Hello World OpenShift"
